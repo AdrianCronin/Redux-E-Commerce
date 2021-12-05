8f95366b3aad2b07c2dc534bb18aef21c7047848
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-12-04 16:35:14 -0800

    created the products slice

--- client/src/utils/productSlice.js
+++ client/src/utils/productSlice.js
@@ -1,1 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = { products: [] };
+
+const productSlice = createSlice({
+    name: 'products',
+    initialState,
+    reducers: {
+        updateProducts(state, action) {
+            state.products = action.payload;
+        }
+    },
+});
+
+export const {updateProducts} = productSlice.actions;
+export default productSlice.reducer;
